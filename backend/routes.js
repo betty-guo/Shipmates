49eefed8cf9afdc185db7a102929438d4b2a5876
rahmagillan <rahmagillan2001@gmail.com>
2021-05-08 19:08:55 -0400

    get and post for active sessions

--- backend/routes.js
+++ backend/routes.js
@@ -48,9 +48,57 @@ routes.route('/users').post((req,res) => {
         });
 });
 
-// Get USER by email
+// GET user by email
 routes.route('/users').get((req,res) => {
     axios.get(`https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v2/keyspaces/dev/users/${req.body.primary_key}`, 
+    {
+        headers: {
+            "X-Cassandra-Token": process.env.ASTRA_DB_APPLICATION_TOKEN,
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            return res.send(response.data);
+        })
+        .catch((err) => {
+            return res.send(err);
+        });
+});
+
+// POST active session to active sessions table
+routes.route('/active-sessions').post((req,res) => {
+    axios.post(`https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v2/keyspaces/dev/active_sessions`, 
+    {
+        id: uuidv4(),
+        url: req.body.url, 
+        deadline: req.body.deadline,
+        current_shipping_price: req.body.current_shipping_price,
+        current_cart_price: req.body.current_cart_price,
+        num_people: req.body.num_people,
+        max_people: req.body.max_people,
+        email: req.body.email,
+        users: req.body.users,
+        items: req.body.items,
+        host: req.body.host,
+        state: req.body.state
+    },
+    {
+        headers: {
+            "X-Cassandra-Token": process.env.ASTRA_DB_APPLICATION_TOKEN,
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            return res.send(response.data);
+        })
+        .catch((err) => {
+            return res.send(err);
+        });
+});
+
+// GET active sessions by id
+routes.route('/active-sessions').get((req,res) => {
+    axios.get(`https://${process.env.ASTRA_DB_ID}-${process.env.ASTRA_DB_REGION}.apps.astra.datastax.com/api/rest/v2/keyspaces/dev/active_sessions/${req.body.primary_key}`, 
     {
         headers: {
             "X-Cassandra-Token": process.env.ASTRA_DB_APPLICATION_TOKEN,
