b8be8dddde3a8d1be2287ea74a9c251c48ea2ac6
ShiHanQin <sh3qin@edu.uwaterloo.ca>
2021-05-09 10:21:48 -0400

    New cards

---
+++ lib/components/hostingSessionCard.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../destination.dart';
+
+class HostingSessionCard extends StatefulWidget {
+  @override
+  _HostingSessionCardState createState() => _HostingSessionCardState();
+}
+
+class _HostingSessionCardState extends State<HostingSessionCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(5),
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            print('Card tapped.');
+          },
+          child: Card(
+            elevation: 10,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Current Shipping Price'),
+                  subtitle: Text('\$' + '257.50'),
+                ),
+                Stack(children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.attach_money),
+                    title: Text('Status'),
+                    subtitle: Text('Procesing'),
+                  ),
+                ]),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
