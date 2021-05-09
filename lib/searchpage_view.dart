cac4008d6c3a9dfc2de1413972f0201ddc4969a7
Betty Guo <bettyg3113@gmail.com>
2021-05-09 10:55:42 -0400

    search page

--- lib/searchpage_view.dart
+++ lib/searchpage_view.dart
@@ -28,70 +28,65 @@ class _SearchPageViewState extends State<SearchPageView> {
       appBar: AppBar(
         title: Text('Search sessions'),
       ),
-      body: FutureBuilder<List<ActiveSessions>> (
+      body: FutureBuilder<List<ActiveSessions>>(
         future: sessionList,
-        builder: (BuildContext context, AsyncSnapshot<List<ActiveSessions>> snapshot) {
-        List<Widget> children;
-        if (snapshot.hasData) {
-    children = <Widget>[ListView (
-    children: <Widget> [
-    Container (
-    margin: EdgeInsets.fromLTRB(20, 30, 0, 0),
-    child: Text(
-    'Looking for something in particular?',
-    textAlign: TextAlign.start,
-    style: TextStyle(
-    fontSize: 20,
-    )),
-    ),
-    Container(
-    padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-    alignment: Alignment.center,
-    child: TextField(
-    decoration: InputDecoration(
-    labelText: 'Search for something!',
-    ),
-
-    ),
-    ),
-    Container (
-    margin: EdgeInsets.fromLTRB(20, 15, 0, 10),
-    child: Text(
-    'Ships near you',
-    textAlign: TextAlign.start,
-    style: TextStyle(
-    fontSize: 20,
-    )),
-    ),
-    Container(
-    width: 500,
-    height: 234,
-    child: SessionCard(),
-    margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-    ),
-    Container(
-    width: 500,
-    height: 234,
-    child: SessionCard(),
-    padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-    ),
-    ]
-    )];
-
-    }
-        else {
-          children = <Widget>[Container()];
-        }
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: children,
-          ),
-        );
-        },),
-
+        builder: (BuildContext context,
+            AsyncSnapshot<List<ActiveSessions>> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            List<ActiveSessions> newSessionList = snapshot.data;
+            children = <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 30, 0, 0),
+                child: Text('Looking for something in particular?',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 20,
+                    )),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                alignment: Alignment.center,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Search for something!',
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(20, 15, 0, 10),
+                child: Text('Ships near you',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 20,
+                    )),
+              ),
+              Container(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: newSessionList.length,
+                  itemBuilder: (context, index) {
+                    //newSessionlist[index]
+                    return Container(
+                      width: 500,
+                      height: 234,
+                      child: SessionCard(),
+                      margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                    );
+                  },
+                ),
+              ),
+            ];
+          } else {
+            children = <Widget>[Container()];
+          }
+          return Center(
+            child: ListView(
+              children: children,
+            ),
+          );
+        },
+      ),
     );
   }
 
