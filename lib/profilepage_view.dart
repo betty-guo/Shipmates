2713592a0bb17ce7cb4fb95ce44add482391b129
ShiHanQin <sh3qin@edu.uwaterloo.ca>
2021-05-08 20:58:05 -0400

    Made user model

--- lib/profilepage_view.dart
+++ lib/profilepage_view.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:tohacks2021/user_model.dart';
 import 'destination.dart';
 
 class ProfilePageView extends StatefulWidget {
@@ -23,16 +28,44 @@ class _ProfilePageViewState extends State<ProfilePageView> {
 
   @override
   Widget build(BuildContext context) {
+    Future<User> fetchUser() async {
+      final res = await http.get(Uri.http('localhost:5000', 'users',
+          {'id': 'be8b0720-922f-40f5-9d5a-0b96c3268f9e'}));
+
+      if (res.statusCode == 200) {
+        return User.fromJson(jsonDecode(res.body)['data'][0]);
+      } else {
+        throw Exception('Failed to get user');
+      }
+    }
+
+    print("HTTP RESPONSE:");
+    print(fetchUser());
+    final List<String> entries = <String>['Name', 'Email', 'Address', 'Rating'];
+    final Map<String, String> values = {
+      'Name:': "Shi Han",
+      'Email:': "sq@gmail.com",
+      "Address": "96 Uni Ave",
+      "Rating": "5/5"
+    };
+
+    Widget _buildList() {
+      return ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(entries[index]),
+              subtitle: Text('${entries[index]}'),
+            );
+          });
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(32.0),
-        alignment: Alignment.center,
-        child: TextField(controller: _textController),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Profile'),
+        ),
+        body: _buildList());
   }
 
   @override
